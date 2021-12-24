14bfebc2824237e1182f3507383936c6d05c7117
Serg <richsphere@gmail.com>
2021-12-25 04:03:05 +0500

    Created handlers module, some files have been modifed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const fortune = require('./lib/fortune')
+// const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
 
 
 const app = express()
@@ -14,26 +15,12 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('/about', (req, res) => {
-    // const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-    res.render('about', {fortune: fortune.getFortune()})
-})
-
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
-
+app.get('/', handlers.home)
+app.get('/about', handlers.about)
+// 404 Page
+app.use(handlers.notFound)
 // 500 Page
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
     `Express running on http://localhost:${port} ` +
