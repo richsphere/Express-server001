d8996a7ad0b0afcfdec9cd4433d1a3beec76fc47
Serg <richsphere@gmail.com>
2022-01-05 02:29:12 +0500

    Files ready to github

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const handlers = require('./lib/handlers')
 
 
 const app = express()
+// Disable no-undef in ESLint
+/* eslint-disable no-undef */
 const port = process.env.PORT || 3030
+/* eslint-enable no-undef */
 
 // Handlebars views
 app.engine('handlebars', expressHandlebars.engine({
@@ -13,14 +16,27 @@ app.engine('handlebars', expressHandlebars.engine({
 }))
 app.set('view engine', 'handlebars')
 
+
+// Disable no-undef in ESLint
+/* eslint-disable no-undef */
 app.use(express.static(__dirname + '/public'))
+/* eslint-enable no-undef */
+
 
 app.get('/', handlers.home)
 app.get('/about', handlers.about)
+//Headers
+app.get('/headers', (req, res) => {
+    res.type('text/plain')
+    const headers = Object.entries(req.headers)
+        .map(([key, value]) => `${key}: ${value}`)
+    res.send(headers.join('\n'))
+})
 // 404 Page
 app.use(handlers.notFound)
 // 500 Page
 app.use(handlers.serverError)
+
 
 if(require.main === module) {
     app.listen(port, () => console.log(
